7161466bad06630a8630a02967b9907e520a9126
Jean Byron Duque <jeduqueca@unal.edu.co>
2024-12-12 14:43:54 -0500

    Creacion migracion products

---
+++ config/programacion_web/resources/views/authors/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Author</h1>
+    <div>
+        <form action="{{route('authors.store')}}" method="post">
+            @csrf
+
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+
+            <label for="nationality">Nacionalidad</label>
+            <input type="text" name="nationality" id="nationality">
+
+            <label for="birth_date">Fecha de nacimiento</label>
+            <input type="date" name="birth_date" id="birth_date">
+
+            <button type="submit">Guardar</button>
+
+        </form>
+    </div>
+    <ul>
+        @foreach ($authors as $author)
+            <li>{{ $author->name }}</li>
+        @endforeach
+    </ul>
+
+</body>
+</html>
---
+++ config/programacion_web/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
